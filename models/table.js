d41dc1d451c7f5b698fcf1655fb8c8d7b4a16f8d
lehieunghia02 <lehieunghia2402@gmail.com>
2025-06-24 00:22:39 +0700

    add(category-api): add CRUD category and middleware authen and author

---
+++ models/table.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const mongoose_delete = require("mongoose-delete");
+
+const tableSchema = new mongoose.Schema(
+ {
+    tableNumber: { type: String, required: true, unique: true },
+    capacity: { type: Number, required: true}, 
+    status: { type: String, enum: ["AVAILABLE", "OCCUPIED", "RESERVED", "MAINTENANCE"], default: "AVAILABLE" },
+    location: {type: String, enum: ["INSIDE", "OUTSIDE", "BALCONY"], default: "INSIDE"},
+    isActive: { type: Boolean, default: true},
+ },
+ {
+  timestamps: true,
+ }
+)
+
+tableSchema.plugin(mongoose_delete, { overrideMethods: "all" });
+const Table = mongoose.model("Table", tableSchema);
+
+module.exports = Table;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const userRouter = express.Router();
 const validateUser = require("../validators/userValidator");
-
+const { isAuthenticated, isAdmin, isStaffOrAdmin } = require("../middleware/auth");
 userRouter.post("/register", validateUser, userController.createUser);
 userRouter.post("/login", userController.loginUser);
 
