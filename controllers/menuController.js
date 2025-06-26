d949b4a8585b2b55fc3b84a0e2707c17bf52f46e
Cuonggkw <cp9188950@gmail.com>
2025-06-26 09:47:18 +0700

    add(menu-api): add api getAllMenu getByIdMenu CreateMenu UpdateMenu DeleteMenu

---
+++ controllers/menuController.js
@@ -0,0 +1,92 @@
+const menuService = require("../services/menuService");
+const { HttpStatusCode } = require("axios");
+
+class MenuController {
+  async getAll(req, res) {
+    try {
+      const menus = await menuService.getAllMenus();
+      return res.status(200).json({
+        status: "success",
+        data: menus,
+      });
+    } catch (error) {
+      return res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message,
+      });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const menu = await menuService.getById(req.params.id);
+      return res.status(200).json({
+        status: "success",
+        data: menu,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      // link ảnh trên cloudinary
+      const image_url = req.body.image_url;
+      const menuData = { ...req.body, image_url };
+      const newMenu = await menuService.create(menuData);
+      return res.status(HttpStatusCode.Created).json({
+        status: "success",
+        data: newMenu,
+        message: "Menu created successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  }
+  async update(req, res) {
+    try {
+      const updatedMenu = await menuService.update(req.params.id, req.body);
+
+      if (!updatedMenu) {
+        return res.status(404).json({
+          status: "error",
+          message: "Menu not found",
+        });
+      }
+      return res.status(200).json(updatedMenu);
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const deletedMenu = await menuService.delete(req.params.id);
+      if (!deletedMenu) {
+        return res.status(404).json({
+          status: "error",
+          message: "Table not found",
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+        message: "Menu deleted successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+  }
+}
+module.exports = new MenuController();
---
+++ models/order.js
@@ -0,0 +1,35 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const mongoose_delete = require("mongoose-delete");
+
+const orderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    table: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table", // Liên kết với bảng table
+    },
+    orderType: {
+      type: String,
+      enum: ["dine-in", "takeaway"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "preparing", "served", "completed", "cancelled"],
+      default: "pending",
+    },
+    isAvailable: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true, // createAt, updateAt
+  }
+);
+
+orderSchema.plugin(mongoose_delete, { overrideMethods: "all" });
+const Order = mongoose.model("order", orderSchema);
+
+module.exports = Order;
